a056eaec7ba110be1068c60b98d0cd7171a3ce4f
Alejandro Gramajo <agramajo@gmail.com>
2021-10-28 21:15:33 -0300

    project

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = '0x3C8C7F06E3662b4bF9217E5377014233D8c1D317';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
